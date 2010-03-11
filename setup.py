c861db34d24c9353ae6eadbff48efac0ee9086f0
subramanian_anand <devnull@localhost>
2010-03-11 15:17:49 +0000

    Added long description

--- setup.py
+++ setup.py
@@ -11,4 +11,21 @@ setup(
     url         = 'http://code.google.com/p/py-pretty/',
     packages    = ['pretty'],
     requires    = ['datetime'],
+    long_description = '''Sample code::
+
+    from datetime import datetime, timedelta
+    now = datetime.now()
+    hrago = now - timedelta(hours=1)
+    yesterday = now - timedelta(days=1)
+    tomorrow = now + timedelta(days=1)
+    dayafter = now + timedelta(days=2)
+
+    import pretty
+    print pretty.date(now)                      # 'now'
+    print pretty.date(hrago)                    # 'an hour ago'
+    print pretty.date(hrago, short=True)        # '1h ago'
+    print pretty.date(hrago, asdays=True)       # 'today'
+    print pretty.date(yesterday, short=True)    # 'yest'
+    print pretty.date(tomorrow)                 # 'tomorrow'
+'''
 )
